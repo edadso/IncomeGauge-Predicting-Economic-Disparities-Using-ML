c31e56c4909b6e9856127244017dddeb8f90ec5d
Emmanuel Dadson <emmanueldadson36@gmail.com>
2024-09-24 21:57:27 +0000

    Modify data page with chunking

--- data.py
+++ data.py
@@ -59,179 +59,173 @@ def show_data():
                 - **household_summary**: Summary of household information.
                 """
                 )
-
-
+            
     if user_choice == "Data Hub":
-        st.markdown("<h1 style='color: lightblue;'> 📚 Data Hub</h1>", unsafe_allow_html=True)
-        st.info(f"### *Welcome To Data Hub*")
-        st.markdown(f"#### **Uploaded data will be availbale for prediction in the predict page**") 
+        st.markdown("<h1 style='color: lightblue;'>📚 Data Hub</h1>", unsafe_allow_html = True)
+        st.info("### *Welcome To Data Hub*")
+        st.markdown("#### **Uploaded data will be available for prediction in the predict page**") 
 
-        with st.expander("## **Explore the dataset used for testing here**", expanded = False, icon = "👇"):
-                # Load data
-                @st.cache_data
-                def load_data():
-                    df = pd.read_csv("data/Test.csv")
-                    return df
-                
-                data = load_data()
+        # Load csv data in chunks
+        @st.cache_data
+        def load_data_in_chunks(file_path, chunk_size, start_chunk):
+            try:
+                chunks = pd.read_csv(file_path, chunksize = chunk_size)
+                for idx, chunk in enumerate(chunks):
+                    if idx == start_chunk:
+                        return chunk
+            except Exception as e:
+                st.error(f"Error loading data: {str(e)}")
+            return None
+        
+        # Load excel data in chunks
+        @st.cache_data
+        def load_xlsx_in_chunks(file_path, chunk_size, start_chunk):
+            try:
+                chunks = pd.read_excel(file_path, chunksize=chunk_size)
+                for idx, chunk in enumerate(chunks):
+                    if idx == start_chunk:
+                        return chunk
+            except Exception as e:
+                st.error(f"Error loading Excel data: {str(e)}")
+            return None
 
-                # Clean columns
-                def clean_columns(data):
-                    # List of columns to drop
-                    columns_to_drop = [
-                                "class", "education_institute", "unemployment_reason", "is_labor_union", 
-                                "occupation_code_main", "under_18_family", "veterans_admin_questionnaire", 
-                                "migration_code_change_in_msa", "migration_prev_sunbelt", 
-                                "migration_code_move_within_reg", "migration_code_change_in_reg", 
-                                "residence_1_year_ago", "old_residence_reg", "old_residence_state"]
-                            
-                    # Drop the specified columns
-                    data = data.drop(columns = columns_to_drop, axis = 1)
+        # Clean dataset
+        def clean_columns(data_chunk):
+            columns_to_drop = ["class", "education_institute", "unemployment_reason", "is_labor_union",
+                            "occupation_code_main", "under_18_family", "veterans_admin_questionnaire",
+                            "migration_code_change_in_msa", "migration_prev_sunbelt", 
+                            "migration_code_move_within_reg", "migration_code_change_in_reg",
+                            "residence_1_year_ago", "old_residence_reg", "old_residence_state"]
 
-                    # Iterate through the columns with category or object (string) data types
-                    for col in data.select_dtypes(include = ["category", "object"]).columns:
-                        data[col] = data[col].str.strip()
+            data_chunk = data_chunk.drop(columns = columns_to_drop, axis=1)
+            for col in data_chunk.select_dtypes(include = ["category", "object"]).columns:
+                data_chunk[col] = data_chunk[col].str.strip()
+            data_chunk.replace("?", np.nan, inplace = True)
+            data_chunk["is_hispanic"] = data_chunk["is_hispanic"].replace("NA", "All other")
+            return data_chunk
 
-                    # Replace "?" with np.nan in the entire DataFrame
-                    data.replace("?", np.nan, inplace = True)
+        # Data preview section
+        with st.expander("## **Explore the dataset used for testing here**", expanded = False):
 
-                    # Replace "NA" with "All other" in the "is_hispanic" column
-                    data["is_hispanic"] = data["is_hispanic"].replace("NA", "All other")
-                            
-                    return data
-                
-                # Display data preview
-                st.data_editor(data)
+            st.sidebar.info("**Explore Test Data Here 👇**")          
+            # Define chunk size
+            chunk_size_1 = 20000
+            # Allow user select page number to access chunked data
+            page_number = st.sidebar.number_input("Select Page", min_value = 0, value = 0, step = 1, key = "page_num_1")
+            data_chunk = load_data_in_chunks("data/Test.csv", chunk_size_1, page_number)
+            
+            if data_chunk is not None:
+                st.write(f"Displaying page {page_number + 1} (Rows {page_number * chunk_size_1 + 1} to {(page_number + 1) * chunk_size_1})")
+                data_chunk_1 = clean_columns(data_chunk)
+                st.dataframe(data_chunk)
 
-                @st.cache_data
-                def describe_data():
-                    return st.write(data.describe()) 
+                if st.sidebar.button("View Stats", key = "data_statistics_1"):
+                    st.write("#### Data Statistics")
+                    st.write(data_chunk.describe())
 
-                @st.cache_data
-                def view_data_shape():              
-                    return st.write(data.shape)
+                if st.sidebar.button("View Data Shape", key = "data_shape_1"):
+                    st.write("#### Data Shape")
+                    st.write(data_chunk.shape)
 
-                @st.cache_data
-                def view_missing_values():
-                    null_count = data.isnull().sum().rename("Missing Value Counts").reset_index()
+                if st.sidebar.button("Count Missing values", key = "missing_values_1"):
+                    st.write("#### Missing Values")
+                    null_count = data_chunk.isnull().sum().reset_index()
                     null_count.columns = ["Features", "Missing Value Counts"]
-                    return st.write(null_count)
+                    st.write(null_count)
 
-                @st.cache_data
-                def view_unique_values():
-                    unique_count = data.nunique().rename("Unique Counts").reset_index()
+                if st.sidebar.button("View Unique Values", key = "unique_values_1"):
+                    st.write("#### Unique Values")
+                    unique_count = data_chunk.nunique().reset_index()
                     unique_count.columns = ["Features", "Unique Counts"]
-                    return st.write(unique_count)
+                    st.write(unique_count)
+            else:
+                st.write("No more data to display.")
 
+        # Upload your dataset
+        with st.expander("**Upload your dataset here**", expanded = False):
+            upload_file = st.file_uploader(label = "Choose a file", type = ["csv", "xlsx"])
 
-                # Button to view data statistics
-                if st.sidebar.button("View Stats"):
-                    st.write(f"#### Data Statistics")
-                    describe_data()
+            if "data_loaded" not in st.session_state:
+                st.session_state.data_loaded = False
 
-                if st.sidebar.button("View Data Shape"):
-                    st.write(f"#### Data Shape")
-                    view_data_shape()
+            if upload_file is not None:
+                st.sidebar.info("**Explore Uploaded Data Here 👇**")
+                try:
+                    if upload_file.name.endswith(".csv"):
+                        file_size = upload_file.size / (1024 * 1024)  # Convert to MB
+                        if file_size > 10:
+                            st.warning("Large file detected! Loading in chunks.")
+                            # Allow the user to choose the chunk size
+                            chunk_size = st.sidebar.slider("Choose chunk size (rows per chunk)", 1000, 25000, 20000, key = "chunk_size_2")
+                            page_number = st.sidebar.number_input("Select Page", min_value = 0, value = 0, step = 1, key = "page_num_2")
+                            data_chunk = load_data_in_chunks(upload_file, chunk_size, page_number)
 
-                if st.sidebar.button("Count Missng values"):
-                    st.write(f"#### Missing Values")
-                    view_missing_values()
-
-                if st.sidebar.button("View Unique Values"):
-                    st.write(f"#### Unique Values")
-                    view_unique_values()
-
-
-        with st.expander("**Upload your dataset here**", expanded = False, icon = "👇"):
-                
-                # Upload file
-                upload_file = st.file_uploader(label="Choose a file", type=["csv", "xlsx"])
-
-                # Initialize session state variable for tracking data load status
-                if "data_loaded" not in st.session_state:
-                    st.session_state.data_loaded = False
-
-                if upload_file is not None:
-                    # Check for file type and read file
-                    try:
-                        if upload_file.name.endswith(".csv"):
-                            data = pd.read_csv(upload_file)
-                        elif upload_file.name.endswith(".xlsx"):
-                            data = pd.read_excel(upload_file)
+                            if data_chunk is not None:
+                                st.write(f"Displaying page {page_number + 1} (Rows {page_number * chunk_size + 1} to {(page_number + 1) * chunk_size})")
+                                data = clean_columns(data_chunk)
+                                st.session_state["uploaded_data"] = data
+                                st.dataframe(data)
                         else:
-                            st.error("Unsupported file type!")
-                            data = None
-                    except Exception as e:
-                        st.error(f"Error reading this file: {str(e)}")
-                        data = None
-                    
-                    if data is not None:                  
-                    
-                        # Clean the columns
-                        data = clean_columns(data)
+                            data = pd.read_csv(upload_file)
+                            data = clean_columns(data)
+                            st.session_state["uploaded_data"] = data
+                            st.subheader("Data Preview")
+                            st.dataframe(data.head())
 
-                        # Store the data in session_state
-                        st.session_state["uploaded_data"] = data
-                        
+                    elif upload_file.name.endswith(".xlsx"):
+                        file_size = upload_file.size / (1024 * 1024)
+                        if file_size > 10:
+                            st.warning("Large file detected! Loading in chunks.")
+                            # Allow the user to choose the chunk size
+                            chunk_size = st.sidebar.slider("Choose chunk size (rows per chunk)", 1000, 25000, 20000, key = "chunk_size_3")
+                            page_number = st.sidebar.number_input("Select Page", min_value = 0, value = 0, step = 1, key = "page_num_3")
+                            data_chunk = load_xlsx_in_chunks(upload_file, chunk_size, page_number)
+
+                            if data_chunk is not None:
+                                st.write(f"Displaying page {page_number + 1} (Rows {page_number * chunk_size + 1} to {(page_number + 1) * chunk_size})")
+                                data = clean_columns(data_chunk)
+                                st.session_state["uploaded_data"] = data
+                                st.dataframe(data)
+                        else:
+                            data = pd.read_excel(upload_file)
+                            data = clean_columns(data)
+                            st.session_state["uploaded_data"] = data
+                            st.subheader("Data Preview")
+                            st.dataframe(data.head())
+
+                except Exception as e:
+                    st.error(f"Error reading this file: {str(e)}")
+
+                if not st.session_state.data_loaded:
+
                     # Display success message
-                    if data is not None and not st.session_state.data_loaded:
-                        success_msg = st.empty()
-                        success_msg.success("Data uploaded successfully!")
-                        # Display success message for 2 seconds
-                        time.sleep(2)
-                        # Clear success message
-                        success_msg.empty()
-                        st.session_state.data_loaded = True
+                    success_msg = st.empty()
+                    success_msg.success("Data uploaded successfully!")
+                    # Display success message for 2 seconds
+                    time.sleep(2)
+                    # Clear success message
+                    success_msg.empty()
+                    st.session_state.data_loaded = True
 
-                    if data is not None:
-                        # Preview data
-                        st.subheader("Data Preview")
-                        st.write("First few rows of your data:")
-                        st.dataframe(data.head())
+                # Extra options for data exploration
+                if st.sidebar.button("View Stats", key = "data_statistics_2"):
+                    st.write("#### Data Statistics")
+                    st.write(data.describe())
 
-                    # Option to display entire data
-                    if st.checkbox("Show entire data (Optional)"):
-                        st.dataframe(data)
+                if st.sidebar.button("View Data Shape", key = "data_shape_2"):
+                    st.write("#### Data Shape")
+                    st.write(data.shape)
 
-                    # Option to explore data
-                    if st.checkbox("Explore data (Optional)"):
-                        @st.cache_data
-                        def describe_data():
-                            return st.write(data.describe()) 
+                if st.sidebar.button("Count Missing values", key = "missing_values_2"):
+                    st.write("#### Missing Values")
+                    null_count = data.isnull().sum().reset_index()
+                    null_count.columns = ["Features", "Missing Value Counts"]
+                    st.write(null_count)
 
-                        @st.cache_data
-                        def view_data_shape():              
-                            return st.write(data.shape)
-
-                        @st.cache_data
-                        def view_missing_values():
-                            null_count = data.isnull().sum().rename("Missing Value Counts").reset_index()
-                            null_count.columns = ["Features", "Missing Value Counts"]
-                            return st.write(null_count)
-
-                        @st.cache_data
-                        def view_unique_values():
-                            unique_count = data.nunique().rename("Unique Counts").reset_index()
-                            unique_count.columns = ["Features", "Unique Counts"]
-                            return st.write(unique_count)
-
-
-                        # Button to view data statistics
-                        if st.sidebar.button("View Stats"):
-                            st.write(f"#### Data Statistics")
-                            describe_data()
-
-                        if st.sidebar.button("View Data Shape"):
-                            st.write(f"#### Data Shape")
-                            view_data_shape()
-
-                        if st.sidebar.button("Count Missng values"):
-                            st.write(f"#### Missing Values")
-                            view_missing_values()
-
-                        if st.sidebar.button("View Unique Values"):
-                            st.write(f"#### Unique Values")
-                            view_unique_values()
-                   
-                else:
-                    st.info("Please upload a file to preview the data.")
+                if st.sidebar.button("View Unique Values", key = "unique_values_2"):
+                    st.write("#### Unique Values")
+                    unique_count = data.nunique().reset_index()
+                    unique_count.columns = ["Features", "Unique Counts"]
+                    st.write(unique_count)
+            else:
+                st.error("Please upload a CSV or XLSX file.")
